940006270b5be67855cc26c5d231cbd8122c1842
shreya-jayani <126176227+shreya-jayani@users.noreply.github.com>
2026-07-01 14:49:28 +0200

    Create SQL database

--- sql/schema/CreateDatabase.sql
+++ sql/schema/CreateDatabase.sql
@@ -1,1 +1,20 @@
+/*
+===========================================================
+Project : Real Estate Business Intelligence Platform
+Author  : Shreya Jayani
+Database: RealEstateBI
+===========================================================
+*/
 
+IF DB_ID('RealEstateBI') IS NOT NULL
+BEGIN
+    ALTER DATABASE RealEstateBI SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE RealEstateBI;
+END
+GO
+
+CREATE DATABASE RealEstateBI;
+GO
+
+USE RealEstateBI;
+GO
